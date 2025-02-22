3dcfb4d3dc84f9c9956e68a186a7a3297385c29e
Clinamenic <ssc@clinamenic.com>
2025-02-22 08:38:31 -0700

    Updated baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "www.openmutualism.xyz",
+    baseUrl: "www.ethereumlocalism.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
